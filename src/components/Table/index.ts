c4d1a5dd944572797c10dabfc940f44aa2902fc7
Tim <tmlyw1@gmail.com>
2025-04-14 04:03:28 +0800

    fixed build issues

--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,6 +1,7 @@
 import Table from './Table';
 import Pagination from './Pagination';
+import type { Column, TableProps, SortDirection, SortState } from '../../types';
 
 export { Pagination };
-export type { Column, TableProps, SortDirection, SortState } from './Table';
+export type { Column, TableProps, SortDirection, SortState };
 export default Table; 
